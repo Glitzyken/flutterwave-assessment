54f4609bdb179f13f04657e87c838b8a35d2a051
Kenneth Jimmy <kenjimmy17@gmail.com>
2021-01-27 14:52:09 +0100

    initialize validation middlewares

--- controllers/validateRuleController.js
+++ controllers/validateRuleController.js
@@ -1,8 +1,22 @@
-exports.getSomething = (req, res) => {
+exports.validateRule = (req, res, next) => {
+  console.log('The RULE fields are required');
+  next();
+};
+
+exports.validatedata = (req, res, next) => {
+  console.log('The DATA fields are required');
+  next();
+};
+
+exports.sendRule = (req, res) => {
+  const payload = req.body;
+
+  console.log(payload.rule.field);
+
   res.status(200).json({
     status: 'success',
     data: {
-      name: 'Kenneth Omonigho Jimmy',
+      payload,
     },
   });
 };
--- routes/validateRuleRoute.js
+++ routes/validateRuleRoute.js
@@ -1,9 +1,15 @@
+const { response } = require('express');
 const express = require('express');
 
 const validateRuleController = require('../controllers/validateRuleController');
 
 const router = express.Router();
 
-router.get('/', validateRuleController.getSomething);
+router.post(
+  '/',
+  validateRuleController.validateRule,
+  validateRuleController.validatedata,
+  validateRuleController.sendRule
+);
 
 module.exports = router;
